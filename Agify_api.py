b288c7eb37fc662bb0dbf889e807bfb767539c5b
Busifad <fadipebusayo@gmail.com>
2024-01-02 19:26:17 +0100

    writing the age prediction to a text file

--- Agify_api.py
+++ Agify_api.py
@@ -3,25 +3,25 @@ from pip._vendor import requests
 from pprint import pprint as pp
 
 def predict_age(name_to_predict):
-
-    agifyurl = f'https://api.agify.io/?name={name_to_predict}'
-
-    response = requests.get(agifyurl)
-    print(response.status_code)
-    data = response.json()
-
-    parameters = {
-        'name': 'name',
-        'label': 'age'
-    }
-
-    response = requests.get(url=agifyurl, params=parameters)
-
-    if response.status_code == 200:
-        age = data['age']
-        print(f"The predicted age is: {age}")
-    else:
-        print(f"Error: {response.status_code}, {response.name}")
-
-name_to_predict = 'Jane'
+    with open('age_predict.txt','w') as text_file:
+        for name in name_to_predict:
+            agifyurl = f'https://api.agify.io/?name={name}'
+            response = requests.get(url=agifyurl)
+ 
+            if response.status_code == 200:
+                data = response.json()
+                pp(data)
+ 
+                message = "At {count} count, the API predicted {name}'s age as: {age}\n".format(
+                    count = data['count'],
+                    name = data['name'],
+                    age = data['age']
+                )      
+                text_file.write(message)
+        # age = data['age']
+        # print(f"The predicted age is: {age}")
+            else:
+                print(f"Error: {response.status_code}, {response.name}")
+ 
+name_to_predict = ['Busayo', 'Jesulolufemi', 'Lolu', 'Ahmed', 'Fadipe', 'Rasheed', 'Janetoms', 'Oketch', 'Oluwatayo', 'Ameh', 'Omobolanle', 'Fatima', 'Janet', 'Blessing', 'Precious']
 predict_age(name_to_predict)
