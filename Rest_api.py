1b3aeef14542842f57111749c176c16d0b5e884b
KateRasheed <kateyliva@gmail.com>
2024-01-05 12:11:32 +0100

    modified:   Rest_api.py

--- Rest_api.py
+++ Rest_api.py
@@ -1,15 +1,7 @@
-# from pip._vendor import requests 
-# from pprint import pprint as pp
-# import json
+from pip._vendor import requests 
+from pprint import pprint as pp
+import json
 
-# restcountries_url = 'https://restcountries.com/v3.1/name/Kenya?fullText=true' 
-
-# response = requests.get(restcountries_url)
-# print(response.status_code)
-# data = response.json()
-
-
-from pip._vendor import requests
 
 def get_country_data_by_name(country_name):
     # Make an HTTP request to the API
